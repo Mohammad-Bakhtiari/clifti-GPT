635bff81333474199d09a2f94160a452e541a0ec
mohammad-bakhtiari <mohammad.bakhtiari.ai@gmail.com>
2026-06-01 16:12:20 +0200

    simplified correction workflow

--- data/correct_batch_effect.sh
+++ data/correct_batch_effect.sh
@@ -33,8 +33,6 @@ for dataset in "${!datasets[@]}"; do
             --query_file "${query_file_prefix}-raw.h5ad"
     echo -e "\e[32mPreprocessing for $dataset completed.\e[0m"
     echo -e "\e[32mBatch effect correction of $dataset.\e[0m"
-    BATCHES="${args[5]}"
-    n_clients=$(echo "$BATCHES" | tr ',' '\n' | wc -l)
     output="${ds_data_dir}/output"
     mkdir -p "$output"
     init_model_path="${output}/model"
@@ -55,37 +53,9 @@ for dataset in "${!datasets[@]}"; do
       --batch_size 50 \
       --remove_cell_types "" \
       --early_stopping_kwargs "{'early_stopping_metric': 'val_loss', 'patience': 20, 'threshold': 0, 'reduce_lr': True, 'lr_patience': 13, 'lr_factor': 0.1}" \
-      --gpu 0 &
-
-
-    echo -e "\e[32mFederated batch effect correction of $dataset.\e[0m"
-    output_path="${output}/federated"
-    mkdir -p "$output_path"
-    fedscgen-federated \
-      --init_model_path "$init_model_path" \
-      --adata "${prep_for_be_datapath}" \
-      --output "$output_path" \
-      --epoch 2 \
-      --batch_key "batch_group" \
-      --cell_key "${celltype_key}" \
-      --batches "$BATCHES" \
-      --lr 0.001 \
-      --batch_size 50 \
-      --hidden_size "800,800" \
-      --z_dim 10 \
-      --early_stopping_kwargs '{"early_stopping_metric": "val_loss", "patience": 20, "threshold": 0, "reduce_lr": True, "lr_patience": 13, "lr_factor": 0.1}' \
-      --batch_out "0" \
-      --remove_cell_types "" \
-      --n_clients "${n_clients}" \
-      --gpu 1 \
-      --n_rounds 8 \
-      --aggregation "fedavg" \
-      --smpc
-
-    wait
+      --gpu 0
 
     echo -e "\e[32mPost processing the corrected data\e[0m"
-
 
     echo -e "\e[32mPost processing the centralized corrected data\e[0m"
     corrected_data_path="${output}/centralized/corrected.h5ad"
@@ -96,15 +66,4 @@ for dataset in "${!datasets[@]}"; do
             --celltype_key "${celltype_key}" \
             --reference_file "${reference_file_prefix}_corrected.h5ad" \
             --query_file "${query_file_prefix}_corrected.h5ad"
-
-
-    echo -e "\e[32mPost processing the federated corrected data\e[0m"
-    corrected_data_path="${output}/federated/fed_corrected.h5ad"
-    python prep_batch_effect_correction.py \
-            --prep_type "post" \
-            --raw_data_path "${ds_data_dir}/${args[1]}" \
-            --corrected_data_path "${corrected_data_path}" \
-            --celltype_key "${celltype_key}" \
-            --reference_file "${reference_file_prefix}_fed_corrected.h5ad" \
-            --query_file "${query_file_prefix}_fed_corrected.h5ad"
 done
